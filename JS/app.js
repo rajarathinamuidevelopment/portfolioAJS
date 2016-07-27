cd874dde497a25ca3f087a85cb33730dfc9ae4cd
rajasearchingjob <rajasearchingjob@gmail.com>
2016-07-26 20:36:10 -0700

    Portfolio in AJS
    My simple portfolio which build in HTML, CSS, Angular JS, Bootstrap,
    JQuery.
    SPA, Custom filter and so on

---
+++ JS/app.js
@@ -0,0 +1,63 @@
+﻿var app = angular.module('myApp', ['ngRoute']);
+
+app.filter('capitalize', function () {
+    return function (input) {
+        return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1) : '';
+    }
+});
+app.config(function ($routeProvider) {
+    $routeProvider
+        // route for the home page
+        .when('/', {
+            templateUrl: 'pages/objectives.html',
+        })
+
+        .when('/objectives', {
+            templateUrl: 'pages/objectives.html',
+        })
+        .when('/Projects', {
+            templateUrl: 'pages/Projects.html',
+        })
+        // route for the Education page
+        .when('/Education', {
+            templateUrl: 'pages/Education.html',
+        })
+         .when('/Achievements', {
+             templateUrl: 'pages/AA.html',
+         })
+        .when('/Personal', {
+            templateUrl: 'pages/Details.html',
+        })
+        // route for the skills page
+        .when('/Skills', {
+            templateUrl: 'pages/skills.html',
+        });
+});
+
+app.controller('profileController', ['$scope', '$http', function ($scope, $http) {
+    $scope.objectives_click= true;
+    $scope.keys = function (obj) {
+        return obj ? Object.keys(obj) : [];
+    }
+    $scope.notSorted = function (obj) {
+        if (!obj) {
+            return [];
+        }
+        return Object.keys(obj);
+    }
+    $scope.objectKeys = function (obj) {
+        return Object.keys(obj);
+    } 
+    $http.get('data/rajarathinam.json').success(function (data) {
+        $scope.profile = data;
+        $scope.artistOrder = 'name';
+    });
+    $scope.name="Raja Rathinam";
+    $scope.jobtitle = "UI & UX Developer";    
+    
+   
+
+}]);
+
+
+
